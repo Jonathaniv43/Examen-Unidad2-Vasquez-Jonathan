d703fa77987bd2a4901c84aae5404ddd71a0c482
Jonathaniv43 <jivasquezf@unah.hn>
2024-04-04 20:41:54 -0600

    Segundo Commit

--- Examen/examen-u2/js/pumagram.js
+++ Examen/examen-u2/js/pumagram.js
@@ -1,15 +1,49 @@
 let contenedor = document.querySelector('.post-container')
 let meGusta = document.querySelector('.like-btn')
+let contador = document.querySelector('.like-count')
+let imagen = document.querySelector('.post-image')
+let comentariosEscritos = document.querySelector('.comments-container')
+let comentarios = document.querySelector('.comment-input')
+
+let btnComentarios= document.querySelector('.add-comment-btn')
 
 meGusta.addEventListener('click', activo)
+imagen.addEventListener('dblclick', activo)
+btnComentarios.addEventListener('click',nComentario)
+
 
 function activo(){
     console.log('activo');
-    meGusta.classList.toggle('activo'); 
-
+    meGusta.classList.toggle('activo')
+    
+    if (meGusta.classList.contains('activo') ) {
+        
+        console.log('activo');
+        meGusta.textContent= 'No me Gusta'
+        // this.textContent = 'No me Gusta'
+        
+        contador.textContent= `${parseInt(contador.textContent)+1}`
+        
+    }else{
+        meGusta.textContent = 'Me gusta'
+        // this.textContent = 'Me gusta'
+        contador.textContent = `${parseInt(contador.textContent)-1}`
+    }
 }
 
-if (activo === 'true'){
-    console.log('boton activo');
+
+function nComentario(){
+    console.log('click en el boton');
+    if (comentarios.innerHTML === ' '){
+        console.log('No hay nada');
+        return;
+    }else{
+        comentariosEscritos.innerHTML += `${comentarios.textContent}`
+    }
 }
+
+
+
+
+
 
